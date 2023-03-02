12d16f80d526cf1c45e752742c9a0013ecaff62e
Elaine <e.cristina00@gmail.com>
2023-03-01 23:21:28 -0300

    Melhorando o código.

--- src/enuns/DiaSemana.java
+++ src/enuns/DiaSemana.java
@@ -2,5 +2,14 @@ package enuns;
 
 public enum DiaSemana {
 
-    DOMIN, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO
+    DOMIN(1), SEGUNDA(2), TERCA(3), QUARTA(4), QUINTA(5), SEXTA(6), SABADO(7);
+
+    private int i;
+    private DiaSemana(int i) {
+        this.i = i;
+    }
+
+    private int getI(){
+        return i;
+    }
 }
