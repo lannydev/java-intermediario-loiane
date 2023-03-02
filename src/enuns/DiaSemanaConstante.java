490d3f258224e40c17ce7fe698f3f25c0196ceea
Elaine <e.cristina00@gmail.com>
2023-03-01 22:48:13 -0300

    Usando constantes para imprimir dia da semana.

--- src/enuns/DiaSemanaConstante.java
+++ src/enuns/DiaSemanaConstante.java
@@ -1,2 +1,12 @@
-package enuns;public class DiaSemanaConstante {
+package enuns;
+
+public class DiaSemanaConstante {
+
+    public static final int DOMINGO = 1;
+    public static final int SEGUNDA_FEIRA = 2;
+    public static final int TERCA_FEIRA = 3;
+    public static final int QUARTA_FEIRA = 4;
+    public static final int QUINTA_FEIRA = 5;
+    public static final int SEXTA_FEIRA = 6;
+    public static final int SABADO= 7;
 }
--- src/enuns/DiaSemanaTeste.java
+++ src/enuns/DiaSemanaTeste.java
@@ -1,2 +1,57 @@
-package enuns;public class DiaSemanaTeste {
+package enuns;
+
+public class DiaSemanaTeste {
+    public static void main(String[] args) {
+
+        imprimeConstantes();
+
+        System.out.println("");
+
+        imprimeDiaSemana(1);
+
+    }
+
+    private static void imprimeConstantes(){
+        int domingo = DiaSemanaConstante.DOMINGO;
+        int segundaFeira = DiaSemanaConstante.SEGUNDA_FEIRA;
+        int tercaFeira = DiaSemanaConstante.TERCA_FEIRA;
+        int quartaaFeira = DiaSemanaConstante.QUARTA_FEIRA;
+        int quintaFeira = DiaSemanaConstante.QUINTA_FEIRA;
+        int sextaFeira = DiaSemanaConstante.SEXTA_FEIRA;
+        int sabado = DiaSemanaConstante.SABADO;
+
+        System.out.println(domingo);
+        System.out.println(segundaFeira);
+        System.out.println(tercaFeira);
+        System.out.println(quartaaFeira);
+        System.out.println(quintaFeira);
+        System.out.println(sextaFeira);
+        System.out.println(sabado);
+    }
+
+    private static void imprimeDiaSemana(int dia){
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda Feira");
+                break;
+            case 3:
+                System.out.println("Terça Feira");
+                break;
+            case 4:
+                System.out.println("Quarta Feira");
+                break;
+            case 5:
+                System.out.println("Quinta Feira");
+                break;
+            case 6:
+                System.out.println("Sexta Feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+        }
+    }
 }
